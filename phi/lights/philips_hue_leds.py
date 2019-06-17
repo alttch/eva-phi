2243976668792878b88ad90f84aa51fdaba33481
divisor <div@altertech.com>
2019-06-17 15:30:08 +0200

    ver

--- phi/lights/philips_hue_leds.py
+++ phi/lights/philips_hue_leds.py
@@ -1,7 +1,7 @@
 __author__ = "Altertech Group, https://www.altertech.com/"
 __copyright__ = "Copyright (C) 2012-2019 Altertech Group"
 __license__ = "Apache License 2.0"
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __description__ = "Philips HUE LEDs"
 
 __equipment__ = 'Philips HUE LEDs'
